aec1be8a4a12088698df1ea08dc088ce4662c31e
Sasha <alex0002b@gmail.com>
2023-08-25 15:49:20 +0100

    initial commit

---
+++ Logger.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace uLog;
+public class Logger {
+
+	private StreamWriter stdout;
+	private StringBuilder builder = new();
+
+	public Logger(Stream output) { stdout = new StreamWriter(output); }
+	public Logger() { stdout = new StreamWriter(Console.OpenStandardOutput()); }
+	
+	public void Log(Severity severity, string message, string sender) {
+		builder.Clear();
+		builder.AppendLine();
+		builder.Append(DateTime.Now.ToShortTimeString());
+		builder.Append(" [");
+		builder.Append(GetSeverityName(severity));
+		builder.Append("] ");
+		builder.Append(sender);
+		builder.Append(": ");
+		builder.Append(message);
+		
+		stdout.WriteLine(builder.ToString());
+	}
+
+	public void Log(Severity severity, string message) {
+		builder.Clear();
+		builder.AppendLine();
+		builder.Append(DateTime.Now.ToShortTimeString());
+		builder.Append(" [");
+		builder.Append(GetSeverityName(severity));
+		builder.Append("] ");
+		builder.Append(message);
+		
+		stdout.WriteLine(builder.ToString());
+	}
+
+	public void Log(string message, string sender) {
+		Log(Severity.Info, message, sender);
+	}
+
+	public void Log(string message) {
+		Log(Severity.Info, message);
+	}
+
+	private string GetSeverityName(Severity severity) =>
+		severity switch {
+			Severity.Debug    => "Debug",
+			Severity.Info     => "Info",
+			Severity.Warning  => "Warn",
+			Severity.Error    => "Error",
+			Severity.Critical => "Critical",
+			_                 => throw new ArgumentOutOfRangeException(nameof(severity), severity, null)
+		};
+}
---
+++ Severity.cs
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 Sasha Beam, All Rights Reserved
+
+namespace uLog;
+
+public enum Severity {
+	Debug,
+	Info,
+	Warning,
+	Error,
+	Critical
+}
