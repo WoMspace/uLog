f34dcbfeba32ed2c5196e47055708bacdb553f88
Sasha <alex0002b@gmail.com>
2023-09-19 04:05:09 +0100

    documentation

--- Logger.cs
+++ Logger.cs
@@ -1,48 +1,116 @@
-﻿using System.Text;
+﻿namespace uLog;
 
-namespace uLog;
 public class Logger {
 
-	private StreamWriter stdout;
-	private StringBuilder builder = new();
+	/// <summary>
+	/// A list of streams that will be written to.
+	/// </summary>
+	/// <para>
+	/// For example, writing to both the console and a log file.
+	/// </para>
+	private List<StreamWriter> outputs = new();
+	/// <summary>
+	/// Severities less important than this will not be logged.
+	/// </summary>
+	public Severity LogLevel { get; set; } = Severity.Info;
+	/// <summary>
+	/// Whether to include the date with the log timestamp.
+	/// </summary>
+	public bool IncludeDate { get; set; } = false;
 
-	public Logger(Stream output) { stdout = new StreamWriter(output); }
-	public Logger() { stdout = new StreamWriter(Console.OpenStandardOutput()); }
+	/// <summary>
+	/// Initialise a new instance of the <c>Logger</c> class that will only output to the console.
+	/// </summary>
+	/// <para>
+	/// Additional stream outputs can still be added later.
+	/// </para>
+	public Logger() {
+		outputs.Add(new StreamWriter(Console.OpenStandardOutput()));
+	}
+	/// <summary>
+	/// Initialise a new instance of the <c>Logger</c> class that will only output to the given stream.
+	/// </summary>
+	/// <para>
+	/// This effectively changes the default stream logging occurs to from the console to the given stream.
+	/// </para>
+	/// <param name="output">The stream to initialise logging to.</param>
+	public Logger(Stream output) {
+		outputs.Add(new StreamWriter(output));
+	}
+	/// <summary>
+	/// Initialise a new instance of the <c>Logger</c> class that will output to every stream in the given array.
+	/// </summary>
+	/// <param name="outputs">An array of <c>Stream</c> objects that events should be logged to.</param>
+	public Logger(Stream[] outputs) {
+		foreach(var stream in outputs) {
+			this.outputs.Add(new StreamWriter(stream));
+		}
+	}
+
 	
+	/// <summary>
+	/// Add an additional output to log events to.
+	/// </summary>
+	/// <param name="output">The <c>Stream</c> object to add.</param>
+	public void AddOutput(Stream output) => outputs.Add(new StreamWriter(output));
+
+
+	/// <summary>
+	/// Log an event.
+	/// </summary>
+	/// <param name="severity">The severity of the event.</param>
+	/// <param name="message">The log message.</param>
+	/// <param name="sender">The internal category of the event.</param>
 	public void Log(Severity severity, string message, string sender) {
-		builder.Clear();
-		builder.AppendLine();
-		builder.Append(DateTime.Now.ToShortTimeString());
-		builder.Append(" [");
-		builder.Append(GetSeverityName(severity));
-		builder.Append("] ");
-		builder.Append(sender);
-		builder.Append(": ");
-		builder.Append(message);
-		
-		stdout.WriteLine(builder.ToString());
+		if(severity >= LogLevel) {
+			string line = $"{FormatDate(DateTime.Now)} [{GetSeverityName(severity)}] {sender}: {message}";
+			WriteOutput(line);
+		}
 	}
 
+	/// <summary>
+	/// Log an event.
+	/// </summary>
+	/// <param name="severity">The severity of the event.</param>
+	/// <param name="message">The log message.</param>
 	public void Log(Severity severity, string message) {
-		builder.Clear();
-		builder.AppendLine();
-		builder.Append(DateTime.Now.ToShortTimeString());
-		builder.Append(" [");
-		builder.Append(GetSeverityName(severity));
-		builder.Append("] ");
-		builder.Append(message);
-		
-		stdout.WriteLine(builder.ToString());
+		if(severity >= LogLevel) {
+			string line = $"{FormatDate(DateTime.Now)} [{GetSeverityName(severity)}] {message}";
+			WriteOutput(line);
+		}
 	}
 
-	public void Log(string message, string sender) {
-		Log(Severity.Info, message, sender);
+	
+	
+	/// <summary>
+	/// Write the given string to all attached outputs.
+	/// </summary>
+	/// <param name="line">The message to write.</param>
+	private void WriteOutput(string line) {
+		foreach(var writer in outputs) {
+			writer.WriteLine(line);
+		}
 	}
 
-	public void Log(string message) {
-		Log(Severity.Info, message);
+	/// <summary>
+	/// Format the given <c>DateTime</c> to a human-readable string, respecting <c>IncludeDate</c>.
+	/// </summary>
+	/// <param name="time">The time to format.</param>
+	/// <returns>A string containing the formatted time and optionally date.</returns>
+	private string FormatDate(DateTime time) {
+		if(IncludeDate) {
+			return $"{time.Year:0000}-{time.Month:00}-{time.Day:00} {time.Hour:00}:{time.Minute:00}:{time.Second:00}";
+		} else {
+			return $"{time.Hour:00}:{time.Minute:00}:{time.Second:00}";
+		}
 	}
 
+	/// <summary>
+	/// Return the human-readable name of the given <c>Severity</c>.
+	/// </summary>
+	/// <param name="severity">The <c>Severity</c> to determine the name of.</param>
+	/// <returns>A string containing the human-readable name of the <c>Severity</c>.</returns>
+	/// <exception cref="ArgumentOutOfRangeException">Thrown if the severity does not exist.</exception>
 	private string GetSeverityName(Severity severity) =>
 		severity switch {
 			Severity.Debug    => "Debug",
--- Severity.cs
+++ Severity.cs
@@ -3,9 +3,24 @@
 namespace uLog;
 
 public enum Severity {
+	/// <summary>
+	/// Verbose information that is useful for diagnosing unexpected behaviour.
+	/// </summary>
 	Debug,
+	/// <summary>
+	/// Fairly verbose information that shows running behaviour.
+	/// </summary>
 	Info,
+	/// <summary>
+	/// Information that may indicate a problem.
+	/// </summary>
 	Warning,
+	/// <summary>
+	/// Information that strongly indicates a problem.
+	/// </summary>
 	Error,
+	/// <summary>
+	/// Equivalent to an aircraft's black box post-crash.
+	/// </summary>
 	Critical
 }
